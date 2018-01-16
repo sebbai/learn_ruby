442ba12fa006699e10d6c3c7464357611edcb16e
bseb <bailly93@gmail.com>
2018-01-17 00:00:28 +0100

    commit ex04 code incomplet à partir de translate cherry

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
-#write your code here
+def translate(word)
+	vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+	#Désolé la ligne suivante est longeur je n'est pas trouver mieux.
+	consonants = ['q', 'w', 'r', 't', 'p', 'l', 'k', 'j', 'h', 'g', 'f', 'd', 's', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+	word_array = word.split
+	word_array2 = word_array.map do |a|
+		if vowels.include?(a[0])
+			a + "ay"
+		elsif consonants.include?(a[0]) && consonants.include?(a[1])
+			puts "methode non trouver "
+		else
+			a.reverse + "ay"
+		end
+	end
+	return word_array2.join()
+end
+puts translate("cherry")
