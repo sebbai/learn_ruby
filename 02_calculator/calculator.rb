3e57fd5c74073d952cf0170b245f8210fed99a32
bseb <bailly93@gmail.com>
2018-01-16 15:32:15 +0100

    1er commit ex02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(nb1, nb2)
+	sum = nb1 + nb2
+	return sum
+end
+
+def subtract(nb1, nb2)
+	return nb1 - nb2
+end
+
+def sum(array)
+	array.inject(0) do |suum, v|
+	suum + v
+end
+
+end
